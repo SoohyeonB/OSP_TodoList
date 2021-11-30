70953a9b812e68a0d64d825d714ff4dff20954db
Soohyeon B <cabbage9963@gmail.com>
2021-11-30 11:24:05 +0900

    Revert "마이페이지"
    This reverts commit b39c42168c928001bf7496dd8b37f59dfe4dc2dc.

--- App.js
+++ App.js
@@ -1,2 +1,46 @@
-import App from './src/App';
+// import App from './src/App';
+// export default App;
+
+
+/*
+* 임시 main 화면 - 수현
+* */
+
+import React from "react";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from "@react-navigation/drawer";
+
+import HomeScreen from '../screens/HomeScreen';
+import MyPageScreen from '../screens/MyPageScreen';
+import {DrawerContent} from './screens/DrawerContent';
+
+
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    Button,
+    StatusBar,
+} from 'react-native';
+
+
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const App =()=> {
+    return(
+        //오른쪽으로 밀면 나오는 navigator
+        <NavigationContainer>
+            <Drawer.Navigator drawerContent={props => <DrawerContent {...props}/>}>
+                <Drawer.Screen name="Home" component={MyPageScreen}/>
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+};
+
 export default App;
+
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+/*
+* 임시 main 화면 - 수현
+* */
+
+import React from "react";
+import {View, Text, Button, StyleSheet, StatusBar} from "react-native";
+
+
+//temp home
+const HomeScreen =({navigation}) => {
+    return (
+        <View style={{ flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text>Temporary Home Screen</Text>
+            <Button
+                title="Go to myPage"
+                onPress={() => navigation.navigate("MyPageScreen")} //myPage 연결
+            />
+        </View>
+    );
+};
+
+
+export default HomeScreen;
+
+
